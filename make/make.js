d42dee8bc17d5556f8ffbf10278c20213db5a053
helpthewalrus <alinatylets@gmail.com>
2018-10-28 17:46:24 +0300

    feat: create function make
    function collects arguments till function for work work arguments is provided

---
+++ make/make.js
@@ -0,0 +1,12 @@
+const make = function make(...arg) {
+  let a = arg;
+
+  const inner = function inner(...args) {
+    if (typeof args[0] !== 'function') {
+      a = a.concat(args);
+      return inner;
+    }
+    return a.reduce(args[0]);
+  };
+  return inner;
+};
--- sum-of-others/sumOfOther.js
+++ sum-of-others/sumOfOther.js
@@ -2,5 +2,3 @@ const sumOfOther = function sumOfOther(array) {
   const reduced = array.reduce((a, b) => a + b);
   return array.map(item => reduced - item);
 };
-
-sumOfOther([1, 2, 3, 4]);
